f94756a62e70e42998395129f77cdc7c227ac6d0
“Yoel <yoelngl321@gmail.com>
2020-06-08 19:14:01 +0700

    initital commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 let handlers = require('./handlers')
 let cors = require('cors');
 let bodyParser = require('body-parser');
+let port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
 
 app.get('/',(req, res) => res.send({hello: 'Hello World!'}));
 
@@ -10,4 +16,4 @@ app.use(handlers.notFound);
 
 app.use(handlers.errorHandler);
 
-app.listen(8000,() => console.log('server work in PORT 8000'));
+app.listen(port,() => console.log('server work in PORT 8000'));
